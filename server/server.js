6d74f879ad850afe5ca3e66b48dcfc16223fdfb8
HudsonAdjetey <adjetey545@gmail.com>
2024-08-06 13:03:16 -0700

    Schema added graphql ~ dummy json

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+var { graphql, buildSchema } = require("graphql");
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var rootValue = { hello: () => "Hello world!" };
+
+var source = "{ hello }";
+
+graphql({ schema, source, rootValue }).then((response) => {
+  console.log(response);
+});
