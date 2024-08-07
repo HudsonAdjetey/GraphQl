c9d62f1d7eff0303c75e27bff9dc09aa5166e967
HudsonAdjetey <adjetey545@gmail.com>
2024-08-07 06:01:01 -0700

    getting all clients

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,2 +1,45 @@
 // graph ql
-const {} = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+} = require("graphql");
+const { clients, projects } = require("../dummyData");
+
+// set up a client type
+const ClientType = new GraphQLObjectType({
+  name: "Clients",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQuery",
+  fields: {
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve() {
+        return clients;
+      },
+    },
+    client: {
+      type: ClientType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        return clients.find((client) => client.id == args.id);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
